89974981af021ae63b5efe9a592b60111ed5ff4b
Renzhentaxi Baerde <renzhentaxibaerde@gmail.com>
2018-06-15 08:23:58 -0400

    uncommented codes

--- week1/work/day4/setup.sh
+++ week1/work/day4/setup.sh
@@ -4,10 +4,11 @@
 weekDir=../../;
 cp $weekDir/{weather.csv,download_trips.sh,load_trips.R,plot_trips.R} ./;
 
-# ./download_trips.sh
+#download trips
+./download_trips.sh
 
-# #load the trips
-# R <load_trips.R --save
+#load the trips
+R <load_trips.R --no-save
 
-#moves non-essential files to archive folder
+#moves non-essential files to trash folder
 mkdir trash && mv *.csv trash/
