34e5828221e0e7786c20f93779529609121bc608
Renzhentaxi Baerde <renzhentaxibaerde@gmail.com>
2018-06-26 13:00:34 -0400

    added quotes around variable references to make it compatible on ds3 laptop

--- sync.sh
+++ sync.sh
@@ -8,27 +8,49 @@ sourceDir="${currentDir}/week${week}";
 fileDir="${sourceDir}/work/day${day}";
 filename="build.setup";
 
-if [ -d $fileDir ] 
-then
-    cd $fileDir
-    for f in `find . ! -name "build.setup" ! -name "."`
-    do
+if [[ -d $fileDir ]]; then
+    cd "${fileDir}";
+    for f in `find . ! -name "build.setup" ! -name "."`; do
         destDir="${sourceDir}/${f}"
         fromDir="${fileDir}/${f}"
-        if [ -f $fromDir ]; then
-            if [ ! -f $destDir ]; then
+        if [ -f "${fromDir}" ]; then
+            if [[ ! -f $destDir ]]; then
                 echo "new file ${f} detected;";
-                cp $fromDir $destDir;
-                git add $destDir;
-            elif ! cmp -s $fromDir $destDir; then
+                cp "${fromDir}" "${destDir}";
+                git add "${destDir}";
+            elif ! cmp -s "${fromDir}" "${destDir}"; then
                 echo "change in file ${f} detected;";
-                cp $fromDir $destDir;
-                git add $destDir;
+                cp "${fromDir}" "$destDir";
+                git add "${destDir}";
             fi;
-        elif [ ! -d $destDir ]; then 
+        elif [ ! -d "${destDir}" ]; then 
             echo "new folder ${f} detected;";
-            mkdir -p $destDir;
-            git add $destDir;
+            mkdir -p "${destDir}";
+            git add "${destDir}";
         fi;
-    done
+    done;
 fi;
+# if [ -d $fileDir ] 
+# then
+#     cd $fileDir
+#     for f in `find . ! -name "build.setup" ! -name "."`
+#     do
+#         destDir="${sourceDir}/${f}"
+#         fromDir="${fileDir}/${f}"
+#         if [ -f $fromDir ]; then
+#             if [ ! -f $destDir ]; then
+#                 echo "new file ${f} detected;";
+#                 cp $fromDir $destDir;
+#                 git add $destDir;
+#             elif ! cmp -s $fromDir $destDir; then
+#                 echo "change in file ${f} detected;";
+#                 cp $fromDir $destDir;
+#                 git add $destDir;
+#             fi;
+#         elif [ ! -d $destDir ]; then 
+#             echo "new folder ${f} detected;";
+#             mkdir -p $destDir;
+#             git add $destDir;
+#         fi;
+#     done
+# fi;
