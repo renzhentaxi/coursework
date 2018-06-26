7d3f292341344883fe358f4d88596aaa13e46b48
Renzhentaxi Baerde <renzhentaxibaerde@gmail.com>
2018-06-26 13:01:06 -0400

    removed commented out codes

--- sync.sh
+++ sync.sh
@@ -30,27 +30,3 @@ if [[ -d $fileDir ]]; then
         fi;
     done;
 fi;
-# if [ -d $fileDir ] 
-# then
-#     cd $fileDir
-#     for f in `find . ! -name "build.setup" ! -name "."`
-#     do
-#         destDir="${sourceDir}/${f}"
-#         fromDir="${fileDir}/${f}"
-#         if [ -f $fromDir ]; then
-#             if [ ! -f $destDir ]; then
-#                 echo "new file ${f} detected;";
-#                 cp $fromDir $destDir;
-#                 git add $destDir;
-#             elif ! cmp -s $fromDir $destDir; then
-#                 echo "change in file ${f} detected;";
-#                 cp $fromDir $destDir;
-#                 git add $destDir;
-#             fi;
-#         elif [ ! -d $destDir ]; then 
-#             echo "new folder ${f} detected;";
-#             mkdir -p $destDir;
-#             git add $destDir;
-#         fi;
-#     done
-# fi;
